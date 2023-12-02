9a402ec98c582c20ff57f7ed79259be8ad490def
homuch <homu777@gmail.com>
2023-12-02 19:28:29 +0800

    bug fix

--- utils/generateProblem.ts
+++ utils/generateProblem.ts
@@ -1,7 +1,13 @@
 const generateProblem = (type: number, count: number) =>
   [...Array(count)].map(() => {
-    const a = Math.floor(Math.random() * 100);
-    const b = Math.floor(Math.random() * 100);
+    const a =
+      type !== 3
+        ? Math.floor(Math.random() * 100)
+        : Math.floor(Math.random() * 32);
+    const b =
+      type !== 3
+        ? Math.floor(Math.random() * 100)
+        : Math.floor(Math.random() * 32);
     const op =
       type === 1 ? "+" : type === 2 ? (Math.random() < 0.5 ? "+" : "-") : "*";
     const answer = op === "+" ? a + b : op === "-" ? a - b : a * b;
